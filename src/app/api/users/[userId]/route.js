a9ce595d3b563937c505774436626c8a444c2e15
cheveniko <nicolas.baquero@icloud.com>
2023-08-30 22:15:05 -0500

    Feat: add searchParams to userId route

---
+++ src/app/api/users/[userId]/route.js
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+
+export async function GET(req, { params }) {
+  const { searchParams } = new URL(req.url);
+  console.log(searchParams.get("nombre"));
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${params.userId}`
+  );
+  const data = await res.json();
+
+  return NextResponse.json(data);
+}
