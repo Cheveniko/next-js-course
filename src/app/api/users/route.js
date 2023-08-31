8b7c59a14b8a4afb6224733bec8440a6da4042e2
cheveniko <nicolas.baquero@icloud.com>
2023-08-30 22:16:21 -0500

    Feat: add post request to log nombre, apellido

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,12 +1,17 @@
 import { NextResponse } from "next/server";
 
+console.log(process.env.TOKEN);
+console.log(process.env.SECRET_KEY);
+
 export async function GET() {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await res.json();
   return NextResponse.json(data);
 }
 
-export function POST() {
+export async function POST(req) {
+  const { nombre, apellido } = await req.json();
+  console.log(nombre, apellido);
   return NextResponse.json({ message: "Creando datos" });
 }
 
